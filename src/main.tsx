149be0dca6f4852f5ea804b2424dca016d1d6dd1
Janani Gurram <68124448+JG-ctrl@users.noreply.github.com>
2026-03-23 14:34:29 -0400

    feat: enable tailwindcss for vite

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// this is the entry point of the React application, it
+  // imports React and my components
+  // creates the root React component (<App />)
+  // mounts it to the #root div in the HTML (index.html)
+// the entire app is rendered inside of the <div id="root"></div> in index.html
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
